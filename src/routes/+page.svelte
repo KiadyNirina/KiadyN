bd6dc16466b3c74186364f2ab28081792dd910c8
Kiady Nirina <kiady142ram@gmail.com>
2024-10-07 12:55:06 +0300

    update design

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -97,7 +97,7 @@
                 <span><img src="adresse-white.png" alt=""> Antananarivo, Madagascar</span>
                 <span><img src="tel-white.png" alt=""> +261341039490</span>
                 <span><img src="email-white.png" alt=""> kiady142ram@gmail.com</span>
-                <span><img src="linkedin-white.png" alt=""> <a href="linkedin.com/in/kiady-ram-5216592a9/">Kiady Ram</a></span>
+                <span><img src="linkedin-white.png" alt=""> <a href="https://www.linkedin.com/in/kiady-ram-5216592a9/">Kiady Ram</a></span>
                 <span><img src="facebook-white.png" alt=""> <a href="https://www.facebook.com/kiady.rambeloson">Kiady Rambeloson</a></span>
                 <span><img src="github-white.png" alt=""> <a href="https://github.com/KiadyNirina">KiadyNirina</a></span>
             </div>
