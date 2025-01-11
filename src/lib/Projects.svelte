fc09dc1f8474e422e989858c48e6aff0ec13d70b
Kiady Nirina <kiady142ram@gmail.com>
2025-01-11 16:37:36 +0300

    some change

--- src/lib/Projects.svelte
+++ src/lib/Projects.svelte
@@ -1,4 +1,5 @@
 <script>
+    import { slide } from "svelte/transition";
     import Beyond from "./project info/Beyond.svelte";
     import Createcv from "./project info/Createcv.svelte";
     import Datalens from "./project info/Datalens.svelte";
@@ -9,259 +10,209 @@
     import Eboss from "./project info/Eboss.svelte";
     import Mci from "./project info/MCI.svelte";
 
-    let fintrack = false;
-    let datalens = false;
-    let realtalk = false;
-    let mycore = false;
-    let beyond = false;
-    let createcv = false;
-    let eboss = false;
-    let estock = false;
-    let mci = false;
+    let selectedProject = null;
 
-    const handleClickFintrack = () => {
-        fintrack = true;
-    }
+    const projects = {
+        fintrack: Fintrack,
+        datalens: Datalens,
+        realtalk: Realtalk,
+        mycore: Mycore,
+        beyond: Beyond,
+        createcv: Createcv,
+        eboss: Eboss,
+        estock: EStock,
+        mci: Mci
+    };
 
-    const handleClickDatalens = () => {
-        datalens = true;
-    }
-
-    const handleClickRealtalk = () => {
-        realtalk = true;
-    }
-
-    const handleClickMycore = () => {
-        mycore = true;
-    }
-
-    const handleClickBeyond = () => {
-        beyond = true;
-    }
-
-    const handleClickCV = () => {
-        createcv = true;
-    }
-
-    const handleClickEboss = () => {
-        eboss = true;
-    }
-
-    const handleClickEstock = () => {
-        estock = true;
-    }
-
-    const handleClickMCI = () => {
-        mci = true;
-    }
+    const handleClick = (projectName) => {
+        selectedProject = projectName;
+    };
 
     const handleClickReturn = () => {
-        fintrack = false;
-        datalens = false;
-        realtalk = false;
-        mycore = false;
-        beyond = false;
-        createcv = false;
-        eboss = false;
-        estock = false;
-        mci = false;
-    }
+        selectedProject = null;
+    };
 </script>
 
-<div class="">
-    {#if fintrack || datalens || realtalk || mycore || beyond || createcv || eboss || estock || mci}
-        <p id="return" on:click={handleClickReturn}> Return to all</p>
-        {#if mycore}
-            <Mycore/>
-        {:else if beyond}
-            <Beyond/>
-        {:else if fintrack}
-            <Fintrack/>
-        {:else if datalens}
-            <Datalens/>
-        {:else if realtalk}
-            <Realtalk/>
-        {:else if createcv}
-            <Createcv/>
-        {:else if eboss}
-            <Eboss/>
-        {:else if estock}
-            <EStock/>
-        {:else if mci}
-            <Mci/>
-        {/if}
+<div>
+    {#if selectedProject}
+        <p id="return" on:click={handleClickReturn}>Return to all</p>
+        <div in:slide out:slide>
+            {#if projects[selectedProject]}
+                <svelte:component this={projects[selectedProject]} />
+            {/if}
+        </div>
     {:else}
-    <div class="projects">
-        <div class="card2">
-            <img src="image.png" alt="">
-            <div class="info">
-                <h3>FinTrack <span id="date">2024</span></h3>
-                <div class="techno">
-                    <div class="techno2">
-                        <img src="vuejs.png" alt="">
-                        <span>Vuejs</span>
+        <div class="projects">
+            <div class="card2" on:click={() => handleClick('fintrack')}>
+                <img src="image.png" alt="">
+                <div class="info">
+                    <h3>FinTrack <span id="date">2024</span></h3>
+                    <div class="techno">
+                        <div class="techno2">
+                            <img src="vuejs.png" alt="">
+                            <span>Vuejs</span>
+                        </div>
+                        <div class="techno2">
+                            <img src="laravel.png" alt="">
+                            <span>Laravel</span>
+                        </div>
+                        <div class="techno2">
+                            <img src="mysql.png" alt="">
+                            <span>MySQL</span>
+                        </div>
                     </div>
-                    <div class="techno2">
-                        <img src="laravel.png" alt="">
-                        <span>Laravel</span>
-                    </div>
-                    <div class="techno2">
-                        <img src="mysql.png" alt="">
-                        <span>MySQL</span>
-                    </div>
+                    <p>... <span id="seeMore">See more</span></p>
                 </div>
-                <p>... <span id="seeMore" on:click={handleClickFintrack}>See more</span></p>
+            </div>
+
+            <div class="card1" on:click={() => handleClick('datalens')}>
+                <img src="datalens.jpg" alt="">
+                <div class="info">
+                    <h3>DataLens <span id="date">2024</span></h3>
+                    <div class="techno">
+                        <div class="techno2">
+                            <img src="python.png" alt="">
+                            <span>Python</span>
+                        </div>
+                    </div>
+                    <p>DataLens is a data visualization ... <span id="seeMore">See more</span></p>
+                </div>
             </div>
         </div>
 
-        <div class="card1">
-            <img src="datalens.jpg" alt="">
-            <div class="info">
-                <h3>DataLens <span id="date">2024</span></h3>
-                <div class="techno">
-                    <div class="techno2">
-                        <img src="python.png" alt="">
-                        <span>Python</span>
+        <div class="projects">
+            <div class="card2" on:click={() => handleClick('realtalk')}>
+                <img src="real_talk.jpg" alt="">
+                <div class="info">
+                    <h3>Real_Talk <span id="date">2024</span></h3>
+                    <div class="techno">
+                        <div class="techno2">
+                            <img src="svelte.png" alt="">
+                            <span>Svelte</span>
+                        </div>
+                        <div class="techno2">
+                            <img src="laravel.png" alt="">
+                            <span>Laravel</span>
+                        </div>
+                        <div class="techno2">
+                            <img src="mysql.png" alt="">
+                            <span>MySQL</span>
+                        </div>
                     </div>
+                    <p>Real_Talk is a real-time chat platform,... <span id="seeMore">See more</span></p>
                 </div>
-                <p>DataLens is a data visualization ... <span id="seeMore" on:click={handleClickDatalens}>See more</span></p>
             </div>
-        </div>
-    </div>
-
-    <div class="projects">
-        <div class="card2">
-            <img src="real_talk.jpg" alt="">
-            <div class="info">
-                <h3>Real_Talk <span id="date">2024</span></h3>
-                <div class="techno">
-                    <div class="techno2">
-                        <img src="svelte.png" alt="">
-                        <span>Svelte</span>
+    
+            <div class="card1" on:click={() => handleClick('beyond')}>
+                <img src="beyond.PNG" alt="">
+                <div class="info">
+                    <h3>Beyond | <span>freelance</span> <span id="date">2024</span></h3>
+                    <div class="techno">
+                        <div class="techno2">
+                            <img src="wordpress.png" alt="">
+                            <span>WordPress</span>
+                        </div>
                     </div>
-                    <div class="techno2">
-                        <img src="laravel.png" alt="">
-                        <span>Laravel</span>
-                    </div>
-                    <div class="techno2">
-                        <img src="mysql.png" alt="">
-                        <span>MySQL</span>
-                    </div>
+                    <p>The Beyond company showcase site... <span id="seeMore">See more</span></p>
                 </div>
-                <p>Real_Talk is a real-time chat platform,... <span id="seeMore" on:click={handleClickRealtalk}>See more</span></p>
-            </div>
-        </div>
-
-        <div class="card1">
-            <img src="beyond.PNG" alt="">
-            <div class="info">
-                <h3>Beyond | <span>freelance</span> <span id="date">2024</span></h3>
-                <div class="techno">
-                    <div class="techno2">
-                        <img src="wordpress.png" alt="">
-                        <span>WordPress</span>
-                    </div>
-                </div>
-                <p>The Beyond company showcase site... <span id="seeMore" on:click={handleClickBeyond}>See more</span></p>
-            </div>
-        </div>
-    </div>
-
-    <div class="projects">
-        <div class="card2">
-            <img src="mycore.PNG" alt="">
-            <div class="info">
-                <h3>MyCore | <span>freelance</span> <span id="date">2024</span></h3>
-                <div class="techno">
-                    <div class="techno2">
-                        <img src="wordpress.png" alt="">
-                        <span>WordPress</span>
-                    </div>
-                </div>
-                <p>The Mycore company showcase site... <span id="seeMore" on:click={handleClickMycore}>See more</span></p>
             </div>
         </div>
     
-        <div class="card2">
-            <img src="createcv.PNG" alt="">
-            <div class="info">
-                <h3>Create_CV <span id="date">2024</span></h3>
-                <div class="techno">
-                    <div class="techno2">
-                        <img src="svelte.png" alt="">
-                        <span>Svelte</span>
+        <div class="projects">
+            <div class="card2" on:click={() => handleClick('mycore')}>
+                <img src="mycore.PNG" alt="">
+                <div class="info">
+                    <h3>MyCore | <span>freelance</span> <span id="date">2024</span></h3>
+                    <div class="techno">
+                        <div class="techno2">
+                            <img src="wordpress.png" alt="">
+                            <span>WordPress</span>
+                        </div>
                     </div>
+                    <p>The Mycore company showcase site... <span id="seeMore">See more</span></p>
                 </div>
-                <p>CreateCV is a simple and intuitive ... <span id="seeMore" on:click={handleClickCV}>See more</span></p>
             </div>
-        </div>
-    </div>
-
-    <div class="projects">
-        <div class="card1">
-            <img src="eboss.jpg" alt="">
-            <div class="info">
-                <h3>eBoss <span id="date">2024</span></h3>
-                <div class="techno">
-                    <div class="techno2">
-                        <img src="svelte.png" alt="">
-                        <span>Svelte</span>
+        
+            <div class="card2" on:click={() => handleClick('createcv')}>
+                <img src="createcv.PNG" alt="">
+                <div class="info">
+                    <h3>Create_CV <span id="date">2024</span></h3>
+                    <div class="techno">
+                        <div class="techno2">
+                            <img src="svelte.png" alt="">
+                            <span>Svelte</span>
+                        </div>
                     </div>
-                    <div class="techno2">
-                        <img src="django.png" alt="">
-                        <span>Django</span>
-                    </div>
-                    <div class="techno2">
-                        <img src="postgresql.png" alt="">
-                        <span>Postgresql</span>
-                    </div>
+                    <p>CreateCV is a simple and intuitive ... <span id="seeMore">See more</span></p>
                 </div>
-                <p>eBoss is a comprehensive school ... <span id="seeMore" on:click={handleClickEboss}>See more</span></p>
             </div>
         </div>
     
-        <div class="card2">
-            <img src="e_stock.jpg" alt="">
-            <div class="info">
-                <h3>E_Stock <span id="date">2023</span></h3>
-                <div class="techno">
-                    <div class="techno2">
-                        <img src="html.png" alt="">
-                        <span>Html</span>
+        <div class="projects">
+            <div class="card1" on:click={() => handleClick('eboss')}>
+                <img src="eboss.jpg" alt="">
+                <div class="info">
+                    <h3>eBoss <span id="date">2024</span></h3>
+                    <div class="techno">
+                        <div class="techno2">
+                            <img src="svelte.png" alt="">
+                            <span>Svelte</span>
+                        </div>
+                        <div class="techno2">
+                            <img src="django.png" alt="">
+                            <span>Django</span>
+                        </div>
+                        <div class="techno2">
+                            <img src="postgresql.png" alt="">
+                            <span>Postgresql</span>
+                        </div>
                     </div>
-                    <div class="techno2">
-                        <img src="css.png" alt="">
-                        <span>Css</span>
-                    </div>
-                    <div class="techno2">
-                        <img src="php.png" alt="">
-                        <span>PHP</span>
-                    </div>
-                    <div class="techno2">
-                        <img src="mysql.png" alt="">
-                        <span>Mysql</span>
-                    </div>
+                    <p>eBoss is a comprehensive school ... <span id="seeMore">See more</span></p>
                 </div>
-                <p>E_Stock is a comprehensive business ... <span id="seeMore" on:click={handleClickEstock}>See more</span></p>
+            </div>
+        
+            <div class="card2" on:click={() => handleClick('estock')}>
+                <img src="e_stock.jpg" alt="">
+                <div class="info">
+                    <h3>E_Stock <span id="date">2023</span></h3>
+                    <div class="techno">
+                        <div class="techno2">
+                            <img src="html.png" alt="">
+                            <span>Html</span>
+                        </div>
+                        <div class="techno2">
+                            <img src="css.png" alt="">
+                            <span>Css</span>
+                        </div>
+                        <div class="techno2">
+                            <img src="php.png" alt="">
+                            <span>PHP</span>
+                        </div>
+                        <div class="techno2">
+                            <img src="mysql.png" alt="">
+                            <span>Mysql</span>
+                        </div>
+                    </div>
+                    <p>E_Stock is a comprehensive business ... <span id="seeMore">See more</span></p>
+                </div>
             </div>
         </div>
-    </div>
-
-    <div class="projects">
-        <div class="card2">
-            <img src="moncomparateurimmo.PNG" alt="">
-            <div class="info">
-                <h3>MCI <span id="date">2022</span></h3>
-                <div class="techno">
-                    <div class="techno2">
-                        <img src="wordpress.png" alt="">
-                        <span>WordPress</span>
+    
+        <div class="projects">
+            <div class="card2" on:click={() => handleClick('mci')}>
+                <img src="moncomparateurimmo.PNG" alt="">
+                <div class="info">
+                    <h3>MCI <span id="date">2022</span></h3>
+                    <div class="techno">
+                        <div class="techno2">
+                            <img src="wordpress.png" alt="">
+                            <span>WordPress</span>
+                        </div>
                     </div>
+                    <p>The Mon Comparateur Immo - MCI ... <span id="seeMore">See more</span></p>
                 </div>
-                <p>The Mon Comparateur Immo - MCI ... <span id="seeMore" on:click={handleClickMCI}>See more</span></p>
             </div>
         </div>
-    </div>
     {/if}
 </div>
 
@@ -345,6 +296,7 @@
             padding: 10px;
             border-radius: 10px;
             margin-top: 15px;
+            background-color: rgba(255, 255, 255, 0.082);
         }
         .card1 img, .card2 img{
             width: 100%;
