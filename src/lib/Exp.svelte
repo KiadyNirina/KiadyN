95e474bf3c6a0dda69e3cb904b9d3ed05606ec38
Kiady Nirina <kiady142ram@gmail.com>
2025-04-14 20:50:35 +0300

    Implement code changes to enhance functionality and improve performance

--- src/lib/Exp.svelte
+++ src/lib/Exp.svelte
@@ -1,37 +1,34 @@
 <div class="exp">
+    <h2>2025</h2>
+    <div class="content">
+        <h4><img src="/atout.png" alt="">Atout Service Mada <i>(CDI)</i> <span>Nov 2024 - Today</span></h4>
+        <ul>
+            <li>As a PHP developer, I worked on projects using CodeIgniter and Magento, where I was responsible for feature development, module integration, and application maintenance.</li>
+        </ul>
+    </div>
+
     <h2>2024</h2>
     <div class="content">
-        <h4>Freelance <i>(Mycore)</i> <span>2024</span></h4>
+        <h4>Freelance <span>2024</span></h4>
         <ul>
-            <li>Design and development of showcase site with particular attention to visual identity, user experience and ergonomics.</li>
-            <li><b>Techno</b> : WordPress</li>
-            <li><b>Link</b> : <a href="https://test-tc.rf.gd/mycore/">Mycore</a></li>
-        </ul>
-
-        <h4>Freelance <i>(Beyond)</i> <span>2024</span></h4>
-        <ul>
-            <li>Design and development of showcase site with particular attention to visual identity, user experience and ergonomics.</li>
-            <li><b>Techno</b> : WordPress</li>
-            <li><b>Link</b> : <a href="https://test-tc.rf.gd/beyond/">Beyond</a></li>
+            <li><b>Website</b> : <a href="https://test-tc.rf.gd/mycore/">Mycore</a> , <b>Techno</b> : WordPress</li>
+            <li><b>Website</b> : <a href="https://test-tc.rf.gd/beyond/">Beyond</a> , <b>Techno</b> : WordPress</li>
         </ul>
     </div>
 
     <h2>2023</h2>
     <div class="content">
-        <h4>Freelance <i>(TeamCread)</i> <span>2023</span></h4>
+        <h4>Freelance <span>2023</span></h4>
         <ul>
-            <li>Design and development of showcase site with particular attention to visual identity, user experience and ergonomics.</li>
-            <li><b>Techno</b> : WordPress</li>
+            <li><b>Website</b> : private , <b>Techno</b> : WordPress</li>
         </ul>
     </div>
 
     <h2>2022</h2>
     <div class="content">
-        <h4>Freelance <i>(Mon Comparateur Immo - MCI)</i> <span>2022</span></h4>
+        <h4>Freelance <span>2022</span></h4>
         <ul>
-            <li>Design and development of showcase site with particular attention to visual identity, user experience and ergonomics.</li>
-            <li><b>Techno</b> : WordPress</li>
-            <li><b>Link</b> : <a href="https://www.moncomparateur-immo.com/">moncomparateur-immo</a></li>
+            <li><b>Website</b> : <a href="https://www.moncomparateur-immo.com/">moncomparateur-immo</a> , <b>Techno</b> : WordPress</li>
         </ul>
     </div>
 </div>
@@ -64,6 +61,10 @@
     }
     h4 span{
         margin-left: auto;
+    }
+    h4 img{
+        height: 15px;
+        margin-right: 10px;
     }
     a{
         text-decoration: none;
