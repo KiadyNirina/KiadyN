a6a231022e235caa4825f24e200ddc40212bc783
Kiady <kiady142ram@gmail.com>
2025-11-01 14:31:28 +0300

    Refactor mobile menu links to use dynamic rendering for improved maintainability

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -78,46 +78,27 @@
         {#if isMenuOpen}
             <div class="absolute top-full left-0 right-0 bg-white/95 dark:bg-gray-900/95 shadow-lg md:hidden">
                 <div class="flex flex-col items-center py-4 space-y-4">
-                    <a 
-                        href="#about"
-                        class="hover:text-blue-500 transition-colors flex items-center"
-                        on:click={toggleMenu}
-                    >
-                        <Icon icon="ix:about" width="15" class="mr-1" />
-                        À propos
-                    </a>
-                    <a 
-                        href="#projects" 
-                        class="hover:text-blue-500 transition-colors flex items-center"
-                        on:click={toggleMenu}
-                    >
-                        <Icon icon="tabler:code" width="15" class="mr-1" />
-                        Projets
-                    </a>
-                    <a 
-                        href="#skills" 
-                        class="hover:text-blue-500 transition-colors flex items-center"
-                        on:click={toggleMenu}
-                    >
-                        <Icon icon="mdi:tools" width="15" class="mr-1" />
-                        Compétences
-                    </a>
-                    <a 
-                        href="#experiences"
-                        class="hover:text-blue-500 transition-colors flex items-center"
-                        on:click={toggleMenu}
-                    >
-                        <Icon icon="pajamas:work" width="15" class="mr-1" />
-                        Expériences
-                    </a>
-                    <a 
-                        href="#contact" 
-                        class="hover:text-blue-500 transition-colors flex items-center"
-                        on:click={toggleMenu}
-                    >
-                        <Icon icon="hugeicons:contact-02" width="15" class="mr-1" />
-                        Contact
-                    </a>
+                    {#each sections as section}
+                        <a 
+                            href={"#" + section} 
+                            class="flex items-center transition-colors hover:text-blue-500"
+                            class:text-blue-600={activeSection === section}
+                            on:click={toggleMenu}
+                        >
+                            <Icon icon={
+                                section === "about" ? "ix:about" :
+                                section === "projects" ? "tabler:code" :
+                                section === "skills" ? "mdi:tools" :
+                                section === "experiences" ? "pajamas:work" :
+                                "hugeicons:contact-02"
+                            } width="15" class="mr-1"/>
+                            {section === "about" ? "À propos" :
+                            section === "projects" ? "Projets" :
+                            section === "skills" ? "Compétences" :
+                            section === "experiences" ? "Expériences" :
+                            "Contact"}
+                        </a>
+                    {/each}
                 </div>
             </div>
         {/if}
