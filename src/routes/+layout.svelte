766db11aa71838dd43277d129c7f69e824d17969
Kiady <kiady142ram@gmail.com>
2026-06-08 20:25:38 +0300

    Update app.html with SEO meta tags and improve title formatting; add +layout.svelte for structured data and canonical link

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,25 @@
+<script>
+	import { PUBLIC_SITE_URL } from '$env/static/public';
+</script>
+
+<svelte:head>
+	<meta name="author" content="KiadyNirina" />
+	<meta name="publisher" content="KiadyNirina" />
+
+	<link rel="canonical" href={PUBLIC_SITE_URL} />
+
+	<meta property="og:site_name" content="KiadyNirina" />
+
+    <script type="application/ld+json">
+    {JSON.stringify({ 
+            "@context": "https://schema.org",
+            "@type": "Person",
+            "name": "KiadyNirina",
+            "jobTitle": "Développeur Full-Stack",
+            "url": PUBLIC_SITE_URL,
+            "knowsAbout": [ "Svelte", "Vue.js", "PHP", "Python", "Laravel", "Django", "PostgreSQL" ]
+    })}
+    </script>
+</svelte:head>
+
+<slot />
